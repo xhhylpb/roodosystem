9b4968d3b3aaa54fc0311bbb8de51fa8222c5aa9
‘pb.li’ <'18636924029@163.com'>
2017-09-21 15:28:37 +0800

    去了override

--- roodosystem/src/main/java/com/xhhy/interceptor/LoginInterceptor.java
+++ roodosystem/src/main/java/com/xhhy/interceptor/LoginInterceptor.java
@@ -11,7 +11,6 @@ import com.xhhy.bean.UserBean;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
-	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		// TODO Auto-generated method stub
@@ -35,14 +34,12 @@ public class LoginInterceptor implements HandlerInterceptor {
 		}
 	}
 
-	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 		// TODO Auto-generated method stub
